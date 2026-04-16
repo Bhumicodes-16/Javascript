ae838212f06a3569d0cdcd4349979cd3a7ce2416
Bhumicodes-16 <bhumikhushi16@gmail.com>
2026-04-16 19:17:42 +0530

    New files added

---
+++ insert&removeelements.js
@@ -0,0 +1,5 @@
+// document.querySelector(".container").innerHTML
+// document.querySelector(".container").outerHTML
+// document.querySelector(".container").tagName
+// document.querySelector(".container").nodeName
+// document.querySelector(".container").textContent
